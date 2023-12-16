830aa8e561964ae0af5e9b7d0e46cb04396792df
Kambaa <Kambaa@users.noreply.github.com>
2023-12-16 17:13:58 +0300

    fix: action fix tries

--- src/main/java/io/github/kambaa/CheckViaProcess.java
+++ src/main/java/io/github/kambaa/CheckViaProcess.java
@@ -63,6 +63,7 @@ public class CheckViaProcess extends MyBaseMojo {
       }
       return outputBuilder.toString().trim();
     } catch (Exception e) {
+      e.printStackTrace();
       throw new MojoFailureException("Error retrieving git log output", e);
     }
   }
